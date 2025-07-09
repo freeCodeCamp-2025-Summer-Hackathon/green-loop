2fab9283c4912ff4f58e43d8b0998fdbf0c5229a
Abraham Gonzalez <abraham.gm08@gmail.com>
2025-07-09 14:34:39 -0600

    removed the temporary code in group.js for a separate branch

--- ensemble/src/frontend/components/Groups.js
+++ ensemble/src/frontend/components/Groups.js
@@ -1,26 +1,5 @@
-import React, { useEffect, useContext } from "react";
-import { UserDataContext } from "../context/userDataContext";
-import { GroupDataContext } from "../context/groupDataContext";
-import { ThreadContext } from "../context/threadContext";
-import { Link } from "react-router-dom";
-
 function Groups(props, i) {
-  const { logGroupData } = useContext(GroupDataContext);
-  const { logThreadData } = useContext(ThreadContext);
-  const { logUserData } = useContext(UserDataContext);
-
-  useEffect(() => {}, []);
-
-  return (
-    <div className="container">
-      <div>
-        <h3>Groups</h3>
-        <div className="groups">
-          <GroupDataContext />
-        </div>
-      </div>
-    </div>
-  );
+  return <div>This is the groups page</div>;
 }
 
 export default Groups;
