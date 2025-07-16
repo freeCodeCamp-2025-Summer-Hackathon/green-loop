d3de9bd52db79f72ca41a0fd6fff50c5a258a64a
Abraham Gonzalez <abraham.gm08@gmail.com>
2025-07-16 09:52:47 -0600

    working on conditional rendering of dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+} from "@mui/material";
 
-export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+function Dashboard() {
+  const [withGroups, setWithGroups] = useState(true);
+
+  if (withGroups) {
+    return (
+      <Container>
+        <Box>
+          <Typography variant="h2">Ensebmle</Typography>
+          <Typography variant="h3">WithGroups</Typography>
+        </Box>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <Box>
+          <Typography variant="h2">Ensebmle</Typography>
+          <Typography variant="h3">Without groups</Typography>
+        </Box>
+      </Container>
+    );
+  }
 }
